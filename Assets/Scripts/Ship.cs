b3ddda887679aa2fe3b5f54ebb2b411a97d0b9e1
vague315 <vague315@gmail.com>
2018-08-30 17:59:05 -0500

    Ship fires with right trigger press

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -9,15 +9,27 @@ public class Ship : MonoBehaviour, IDamageable {
     public Transform RightWing;
     public Turret turret;
 
+    //TODO move all input stuff to an input class
+    private bool rtPressed;
+    private bool rtHeld;
+
 	void Start() {
 		
 	}
 	
 	void Update() {
-		if(Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Joystick1Button0)){
+		if(Input.GetKeyDown(KeyCode.Mouse0) || GetRightTriggerDown()){
             turret.Fire();
         }
 	}
+
+    private bool GetRightTriggerDown(){
+        float rtInput = Input.GetAxis("XboxRightTrigger");
+        if(rtHeld && rtPressed){ rtPressed = false; }
+        if(!rtHeld && rtInput > 0.5f){ rtPressed = rtHeld = true; }
+        if(rtInput==0){ rtPressed = rtHeld = false; }
+        return rtPressed;
+    }
 
     public void TakeDamage(DamageDealer damageDealer) {
         Health -= damageDealer.DamageAmount;
