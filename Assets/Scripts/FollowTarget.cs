0aaaffdb19838636e9d37e139966f8c6db000859
Seth Farrar <seth@ai-sys.com>
2018-08-30 12:54:21 -0500

    Resizes ship bullet. Adds camera follow

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    public Transform Target;
+    public Vector3 Offset;
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+
+	void LateUpdate () {
+        if(Target==null){ return; }
+        Vector3 targetPosition = Target.position.With(y:transform.position.y) + Offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
+
+}
