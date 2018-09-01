7c7bfc5e143e49396b181c49cab907e8ae89d278
vague315 <vague315@gmail.com>
2018-09-01 16:10:29 -0500

    Begins implementing enemy ship

---
+++ Assets/Scripts/EnemyShip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShip : MonoBehaviour, IDamageable {
+
+	public int Health = 4;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void TakeDamage(DamageDealer damageDealer) {
+		Health -= damageDealer.DamageAmount;
+		if(Health<=0){
+			KillSelf();
+		}
+    }
+
+	private void KillSelf() {
+		Destroy(this.gameObject);
+	}
+
+}
