ebbc822f25ea9bb7ad8d4b81699316de21154438
Seth Farrar <seth@ai-sys.com>
2018-09-06 13:46:52 -0500

    Adds a few different ripple effects

---
+++ Assets/Scripts/Particles/CircleParticleScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleParticleScript : MonoBehaviour {
+
+    public Vector3 speed;
+
+	void Start() {
+		
+	}
+	
+	void Update() {
+        transform.Rotate(speed * Time.deltaTime);
+	}
+}
