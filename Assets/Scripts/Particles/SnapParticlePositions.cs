62e9498965fdaab91777a42ea9c0e1d38b9fbefa
Seth Farrar <seth@ai-sys.com>
2018-09-10 12:53:01 -0500

    Adds invulnerability effect to boss prefab

---
+++ Assets/Scripts/Particles/SnapParticlePositions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class SnapParticlePositions : MonoBehaviour {
+
+    public Transform target;
+
+    private ParticleSystem ps;
+    private ParticleSystem.Particle[] particles;
+
+	void Awake() {
+		ps = GetComponent<ParticleSystem>();
+        particles = new ParticleSystem.Particle[ps.main.maxParticles];
+	}
+	
+	void LateUpdate() {
+        int alive = ps.GetParticles(particles);
+        Debug.Log("alive: " + alive);
+        for(int i=0; i<alive; ++i){
+            particles[i].position = particles[i].position.With(x:target.position.x, z:target.position.z);
+        }
+        ps.SetParticles(particles, alive);
+	}
+}
